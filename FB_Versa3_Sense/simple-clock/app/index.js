c9f5d0e71bd6852da662c134e446f7d8182f800f
Joshua Horvath <jsh_197@hotmail.com>
2026-01-28 21:54:00 -0500

    Base of clock and first 5 minute tick marks

--- FB_Versa3_Sense/simple-clock/app/index.js
+++ FB_Versa3_Sense/simple-clock/app/index.js
@@ -1,1 +1,101 @@
-console.log('Hello world!');
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Joshua Horvath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import clock from "clock";
+import * as document from "document";
+
+// Tick every second
+clock.granularity = "seconds";
+
+// Get a handle on GUI label elements
+let hourHand = document.getElementById("hourHand");
+let minuteHand = document.getElementById("minuteHand");
+let secondHand = document.getElementById("secondHand");
+
+let zeroMinTick = document.getElementById("zeroMinTick");
+let oneMinTick = document.getElementById("oneMinTick");
+let twoMinTick = document.getElementById("twoMinTick");
+let threeMinTick = document.getElementById("threeMinTick");
+let fourMinTick = document.getElementById("fourMinTick");
+let fiveMinTick = document.getElementById("fiveMinTick");
+
+
+setTickMarks();
+
+/**
+ * Rotates the clock hands to show the curent time.
+ */
+function updateClock() {
+  let today = new Date();
+  let hours = today.getHours() % 12;
+  let mins = today.getMinutes();
+  let secs = today.getSeconds();
+
+  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
+  minuteHand.groupTransform.rotate.angle = minutesToAngle(mins);
+  secondHand.groupTransform.rotate.angle = secondsToAngle(secs);
+}
+
+// Update the clock every tick event
+clock.addEventListener("tick", updateClock);
+
+/**
+ * Returns an angle (0-360) for the current hour in the day.
+ * Also adjust the hour hand for minutes past the hour.
+ * @param {*} hours
+ * @param {*} minutes
+ * @returns
+ */
+function hoursToAngle(hours, minutes) {
+  let hourAngle = (360 / 12) * hours;
+  let minAngle = (360 / 12 / 60) * minutes;
+  return hourAngle + minAngle;
+}
+
+/**
+ * Returns an angle (0-360) for minutes
+ * @param {*} minutes
+ * @returns
+ */
+function minutesToAngle(minutes) {
+  return (360 / 60) * minutes;
+}
+
+/**
+ * Returns an angle (0-360) for seconds
+ * @param {*} seconds
+ * @returns
+ */
+function secondsToAngle(seconds) {
+  return (360 / 60) * seconds;
+}
+
+function setTickMarks() {
+  zeroMinTick.groupTransform.rotate.angle = 0;
+  oneMinTick.groupTransform.rotate.angle = 6;
+  twoMinTick.groupTransform.rotate.angle = 12;
+  threeMinTick.groupTransform.rotate.angle = 18;
+  fourMinTick.groupTransform.rotate.angle = 24;
+  fiveMinTick.groupTransform.rotate.angle = 30;
+}
